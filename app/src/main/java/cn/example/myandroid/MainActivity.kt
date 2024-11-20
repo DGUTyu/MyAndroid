b380e3aa4579a6e18655310d72ed67c5b6c61119
luyao <hello_luyao@163.com>
2024-11-20 19:48:31 +0800

    create common library

--- app/src/main/java/cn/example/myandroid/MainActivity.kt
+++ app/src/main/java/cn/example/myandroid/MainActivity.kt
@@ -1,11 +1,18 @@
 package cn.example.myandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import cn.example.common.CommonMainActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<TextView>(R.id.tvHello).setOnClickListener {
+            startActivity(Intent(this, CommonMainActivity::class.java))
+        }
     }
 }
